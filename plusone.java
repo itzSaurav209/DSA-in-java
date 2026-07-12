6c2c3899092cec03567afc46206599a2387efae4
itzSaurav209 <sauravshukla1103@gmail.com>
2026-07-12 12:26:46 +0530

    using arraylist

--- plusone.java
+++ plusone.java
@@ -1,7 +1,8 @@
 // hame list me yaa array me diye gaye number me 1 add karwana hai 
 import java.util.*;
 class plusone {
-
+    // this is done eith the help of array
+    /*
     public static int[] plusOne(int[] digits) {
         int n=digits.length-1; 
         
@@ -17,6 +18,25 @@ class plusone {
         ans[0]=1;
         return ans;
     }
+    */
+   //now i am doing with the help of arraylist
+   static ArrayList<Integer> increment(ArrayList<Integer> arr) {
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+
+            if (arr.get(i) != 9) {
+                arr.set(i, arr.get(i) + 1);
+                return arr;
+            }
+
+            arr.set(i, 0);
+        }
+
+        arr.add(0, 1);
+        return arr;
+    } 
+    // this  main method is for array list
+    /*
     public static void main(String[] args) {
      int [] digits=new int[5];
         Scanner sc= new Scanner(System.in);
@@ -32,5 +52,29 @@ class plusone {
         for (int x : result) {
             System.out.print(x + " ");
     }
+}*/
+//this is from array list 
+ public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        System.out.print("Enter number of digits: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the digits:");
+
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+
+        ArrayList<Integer> ans = increment(arr);
+
+        System.out.println("Answer:");
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
 }
 }
